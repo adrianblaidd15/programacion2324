c951b26da7eaee995df218a3aed5e31b54f8c2b2
Adrian <adrian.troncoso.acosta@gmail.com>
2023-09-28 14:14:57 +0200

    sdd

---
+++ Ejercicios/src/Boletin1_2/Ejercicio15.java
@@ -0,0 +1,24 @@
+package Boletin1_1;
+
+import java.util.Scanner;
+
+public class Ejercicio15 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Dame la primera longitug");
+        double lon1 = scan.nextDouble();
+        scan.nextLine();
+        System.out.println("Dime la segunda longitud");
+        double lon2 = scan.nextDouble();
+        scan.nextLine();
+        System.out.println("Dime la tercera longitud");
+        double lon3 = scan.nextDouble();
+        scan.nextLine();
+
+
+
+    }
+}
+
+
