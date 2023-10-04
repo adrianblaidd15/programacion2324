6fca82580578a8b4c77276a84fb990b68e7c264b
Adrian <adrian.troncoso.acosta@gmail.com>
2023-10-04 11:14:12 +0200

    Ultimo

--- Ejercicios/src/Boletin1_1Java/ejerccio7.java
+++ Ejercicios/src/Boletin1_1Java/ejerccio7.java
@@ -5,16 +5,19 @@ import java.util.Scanner;
 public class ejerccio7 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int cant;
-        int num;
+        int conta;
+        int suma = 0;
+        do {
+            System.out.println("Dame una cantidad de números");
+            conta = scan.nextInt();
+        } while (conta <= 0);
 
-        System.out.println("Escribe la cantidad de números que querias introducir");
-        cant = scan.nextInt();
-
-        while (cant < 0) {
-            System.out.println("La cantidad debe ser positva");
-            cant = scan.nextInt();
+        for (int i = 1; i <= conta; i++) {
+            System.out.println("Dame los siguientes números");
+            int num2 = scan.nextInt();
+            suma += num2;
         }
+        System.out.println("La media te sale " + (double) suma / conta);
 
     }
 }
